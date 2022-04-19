98ce07976cfc1fe8fbd789493b1dc6aa874c897e
Kevin <krefin1@hotmail.com>
2022-04-19 16:34:11 +0200

    update to slash command for multiple filters

--- slash/filter.js
+++ slash/filter.js
@@ -1,59 +1,104 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
+const { MessageEmbed } = require("discord.js")
+
+let list = ['earrape', 'bassboost', 'nightcore', '8D']
+let filters = []
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("filter")
-        .setDescription("Applies an audio filter (bassboost, earrape, nightcore, 8d)")
-        .addStringOption((option) => option.setName("filter").setDescription("the filter name").setRequired(true)),
+        .setDescription("Applies an audio filter (bassboost, earrape, nightcore, 8D, off)")
+        .addStringOption((option) => option.setName("filter").setDescription("the filter name").setRequired(true))
+        .addStringOption((option) => option.setName("filter2").setDescription("the filter name").setRequired(false))
+        .addStringOption((option) => option.setName("filter3").setDescription("the filter name").setRequired(false)),
     run: async ({ client, interaction }) => {
         const queue = client.player.getQueue(interaction.guildId)
 
         if (!queue) return await interaction.editReply("There are no songs in the queue")
 
-        let filter = interaction.options.getString("filter")
-        let reply = "Applied filter: " + filter
-
-        switch(filter) {
-            case "earrape":
-                await queue.setFilters({
-                    earrape: !queue.getFiltersEnabled().includes(filter)
-                });
-                reply = "Applied filter: " + filter
-                if (!queue.getFiltersEnabled().includes(filter)) {
-                    reply = "Disabled filter: " + filter
-                }
-                break;
-            case "nightcore":
-                await queue.setFilters({
-                    nightcore: !queue.getFiltersEnabled().includes(filter)
-                });
-                reply = "Applied filter: " + filter
-                if (!queue.getFiltersEnabled().includes(filter)) {
-                    reply = "Disabled filter: " + filter
-                }
-                break;
-            case "bassboost":
-                await queue.setFilters({
-                    bassboost_high: !queue.getFiltersEnabled().includes('bassboost_high')
-                });
-                reply = "Applied filter: " + filter
-                if (!queue.getFiltersEnabled().includes('bassboost_high')) {
-                    reply = "Disabled filter: " + filter
-                }
-                break;
-            case "8d":
-                await queue.setFilters({
-                    '8D': !queue.getFiltersEnabled().includes('8D')
-                });
-                reply = "Applied filter: " + '8D'
-                if (!queue.getFiltersEnabled().includes('8D')) {
-                    reply = "Disabled filter: " + '8D'
-                }
-                break;
-            default:
-                reply = ("Unknown filter!")
+        filters.push(interaction.options.getString("filter"))
+        if ((interaction.options.getString("filter2")) != null) {
+            filters.push((interaction.options.getString("filter2")))
+        }
+        if ((interaction.options.getString("filter3")) != null) {
+            filters.push((interaction.options.getString("filter3")))
         }
 
-        await interaction.editReply(reply)
+        let available = true
+        let unavailable = ""
+        filters.forEach(element => {
+            if (!list.includes(element.toString())) {
+                available = false
+                unavailable = element.toString()
+            }
+        });
+
+        console.log(available.toString())
+        if (available) {
+            if (filters.length == 1) {
+                if (filters[0] == "off") {
+                    console.log('removing filters...')
+                    await queue.setFilters({
+                        earrape: false,
+                        bassboost: false,
+                        nightcore: false,
+                        '8D': false
+                    }).then(
+                        createEmbed()
+                    )
+                } else {
+                    await queue.setFilters({
+                        [filters[0]]: true,
+                    }).then(
+                        createEmbed()
+                    )
+                }
+            }
+            if (filters.length == 2) {
+                await queue.setFilters({
+                    [filters[0]]: true,
+                    [filters[1]]: true
+                }).then(
+                    createEmbed()
+                )
+            }
+            if (filters.length == 3) {
+                await queue.setFilters({
+                    [filters[0]]: true,
+                    [filters[1]]: true,
+                    [filters[2]]: true
+                }).then(
+                    createEmbed()
+                )
+            }
+        } else {
+            if (unavailable == "off") {
+                console.log('removing filters...')
+                    await queue.setFilters({
+                        earrape: false,
+                        bassboost: false,
+                        nightcore: false,
+                        '8D': false
+                    }).then(
+                        createEmbed()
+                    )
+            } else {
+                await interaction.editReply('Filter does not exist!')
+            }
+        }
+
+        async function createEmbed() {
+            const embed = new MessageEmbed()
+                .setTitle('Active audio filters')
+                .addFields(
+                    { name: 'earrape', value: queue.getFiltersEnabled().includes('earrape').toString() },
+                    { name: 'bassboost', value: queue.getFiltersEnabled().includes('bassboost').toString() },
+                    { name: 'nightcore', value: queue.getFiltersEnabled().includes('nightcore').toString() },
+                    { name: '8D', value: queue.getFiltersEnabled().includes('8D').toString() }
+                )
+            filters = []
+            available = true
+            await interaction.editReply({ embeds: [embed] })
+        }  
     }
 }
