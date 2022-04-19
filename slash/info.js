03ad02a2fbf51f8f05fb443984dd677c666fb73c
Kevin <krefin1@hotmail.com>
2022-04-19 16:36:04 +0200

    use queue.getPlayerTimestamp() now

--- slash/info.js
+++ slash/info.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
-const moment = require("moment")
 
 const index = require('../index')
 
@@ -23,59 +22,6 @@ module.exports = {
         console.log('streamtime' + queue.streamTime)
         console.log('totalTime' + queue.totalTime)
         console.log('getPlayerTimestamp current: ' + queue.getPlayerTimestamp().current + ' end: ' + queue.getPlayerTimestamp().end + ' progress: ' + queue.getPlayerTimestamp().progress)
-        
-        // const currentDate = new Date();
-        // const dateStart = index.date
-        // let secondsDiff = moment(currentDate).diff(moment(dateStart), 'seconds')
-
-        // let h = 0
-        // let m = 0
-        // let s = 0
-
-        // if (Math.floor(secondsDiff / 3600) > 0) {
-        //     if (Math.floor(secondsDiff / 3600 > 100)) {
-        //         secondsDiff = 0
-        //     } else {
-        //         h = Math.floor(secondsDiff / 3600)
-        //         secondsDiff = secondsDiff - (h * 3600)
-        //     }
-        // }
-        // if (Math.floor(secondsDiff / 60) > 0) {
-        //     m = Math.floor(secondsDiff / 60)
-        //     secondsDiff = secondsDiff - (m * 60)
-        // }
-        // s = secondsDiff
-
-        // m = (m<10) ? '0' + m : m;
-        // s = (s<10) ? '0' + s : s;
-
-        // let duration = queue.current.duration
-        // console.log(duration)
-        // let split = duration.split(':')
-
-        // let dh = 0
-        // let dm = 0
-        // let ds = 0
-
-        // if (split.length > 2) {
-        //     dh = split[0]
-        //     dm = split[1]
-        //     ds = split[2]
-        //     if (dh < 10) {
-        //         dh = dh.substring(1)
-        //     }
-        // } else {
-        //     dm = split[0]
-        //     ds = split[1]
-        // }
-        // if (dm < 10) {
-        //     if (dm.length < 2) {
-        //         dm = (dm<10) ? '0' + dm : dm;
-        //     }
-        // }
-
-        // var playTime =  h + ':' + m + ':' + s
-        // var dTime =  dh + ':' + dm + ':' + ds
 
         const currentTime = queue.getPlayerTimestamp().current
         const endTime = queue.getPlayerTimestamp().end
